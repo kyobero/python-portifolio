10b80dff6b5945366cbaed225ef122e0d60303ff
Ronald <kyoberonald@gmail.com>
2019-12-13 09:34:57 +0000

    python manage.py collectstatic

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from.models import Job
 
 # Create your views here.
 
-def nick(request):
-    return render (request, 'jobs/home.html')
+def homepage(request):
+    jobs = Job.objects
+    # jobs = Job.objects
+    return render (request, 'jobs/home.html', {'jobs':jobs})
--- portfolio-project/portfolio/urls.py
+++ portfolio-project/portfolio/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('kyobe', jobs.views.home, name='kyobe'),
-     path('', jobs.views.nick, name='nick'),
-]
+     path('', jobs.views.homepage, name='home'),
+    
+    ]+ static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
